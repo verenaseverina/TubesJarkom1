8699a8cb688685451b39ebcca64d794c5e8db4bf
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-10-14 13:11:33 +0700

    Modify packet and ACK structure

--- src/ack.h
+++ src/ack.h
@@ -1,8 +1,9 @@
 #pragma once
 
-typedef struct ACK {
-    Byte ACK;
-    unsigned int next_seqnum;
-    Byte adv_windsize;
-    Byte checksum;
+typedef struct {
+    char ACK; // ACK
+    uint32_t next_seqnum; // next sequence number
+    uint8_t adv_windsize; // advertised window size
+    uint8_t checksum; // checksum
 }
+ack;
--- src/packet.h
+++ src/packet.h
@@ -1,13 +1,10 @@
 #pragma once
 
-typedef unsigned char Byte;
-
-typedef struct MESSG
-{
-    Byte soh;
-    unsigned int seqnum;
-    unsigned int stx;
-    unsigned int etx;
-    Byte checksum;
-    Byte *data;
-} MESSG;
+typedef struct {
+    char SOH; // start of header
+    uint32_t SeqNum; // sequence number of packet
+    char STX; // start of text
+    char data; // data
+    char ETX; // end of text
+    uint8_t checksum; // checksum
+} packet;
